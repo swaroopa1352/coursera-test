22ae12f1e40cdaae791c50cf6fad4c6265045078
Your Name <you@example.com>
2021-12-12 12:33:16 +0530

    module4 assignment.

---
+++ module4-solution/script.js
@@ -0,0 +1,20 @@
+var names=new Array();
+names[0]="Ben";
+names[1]="Andrew";
+names[2]="Jhon";
+names[3]="Priya";
+names[4]="Swaroopa";
+names[5]="Yaakov";
+names[6]="Jen";
+names[7]="jimin";
+names[8]="Suma";
+names[9]="jim";
+
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+         console.log("Goodbye "+ names[i]);
+	}
+	else{
+		 console.log("Hello "+ names[i]);
+	}
+}
